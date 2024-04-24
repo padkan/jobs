6d687ad293251ff626a7bbda2c905ba883c09dd6
saeed padkan <s.padkan@gmail.com>
2024-04-24 22:51:52 +0200

    add more test for navigation item menu

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -5,9 +5,24 @@
       <div
         class="border- mx-auto flex h-full flex-nowrap border-b border-solid border-x-brand-gray-1 px-8"
       >
-        <a href="/" class="flex h-full items-center text-xl font-semibold">{{
-          company
-        }}</a>
+        <a
+          v-bind:href="url"
+          class="flex h-full items-center text-xl font-semibold"
+          >{{ company }}</a
+        >
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li
+              v-for="menuItem in menuItems"
+              v-bind:key="menuItem"
+              class="ml-9 h-full first:ml-0"
+            >
+              <a href="" class="flex h-full items-center py-2.5">{{
+                menuItem
+              }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -20,7 +35,16 @@ export default {
   name: 'mainNav',
   data() {
     return {
-      company: 'Next Job!'
+      company: 'Next Job!',
+      url: 'http://localhost:3000/test',
+      menuItems: [
+        'Teams',
+        'Locations',
+        'Life at Next Job',
+        'How we hire',
+        'Students',
+        'Jobs'
+      ]
     };
   }
 };
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -5,6 +5,23 @@ import MainNav from '@/components/MainNav.vue';
 describe('MainNav', () => {
   it('display company name', () => {
     render(MainNav);
-    screen.getByText('Next Job!');
+    const companyName = screen.getByText('Next Job!');
+    expect(companyName).toBeInTheDocument();
+  });
+
+  it('display menu items for navigation', () => {
+    render(MainNav);
+    const navigationMenuItems = screen.getAllByRole('listitem');
+    const navigationMenuTexts = navigationMenuItems.map(
+      (item) => item.textContent
+    );
+    expect(navigationMenuTexts).toEqual([
+      'Teams',
+      'Locations',
+      'Life at Next Job',
+      'How we hire',
+      'Students',
+      'Jobs'
+    ]);
   });
 });
