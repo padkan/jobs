0b88b24f02106bf667ec4ec12076664739ceb409
saeed padkan <s.padkan@gmail.com>
2024-05-01 23:01:29 +0200

    add pagination with test

--- frontend/src/components/JobResults/JobListings.vue
+++ frontend/src/components/JobResults/JobListings.vue
@@ -1,8 +1,39 @@
 <template>
   <main class="flex-auto bg-brand-gray-2 p-8">
     <ol>
-      <job-listing v-for="job in jobs" :key="job.id" :job="job" />
+      <job-listing v-for="job in displayedJobs" :key="job.id" :job="job" />
     </ol>
+    <div class="mx-auto mt-8">
+      <div class="flex flex-row flex-nowrap">
+        <p class="flex-grow text-sm">Page {{ currentPage }}</p>
+        <div class="flex items-center justify-center">
+          <router-link
+            v-if="previousPage"
+            role="link"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+            :to="{
+              name: 'JobResult',
+              query: {
+                page: previousPage
+              }
+            }"
+            >Previous</router-link
+          >
+          <router-link
+            role="link"
+            v-if="nextPage"
+            :to="{
+              name: 'JobResult',
+              query: {
+                page: nextPage
+              }
+            }"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+            >Next
+          </router-link>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 <script>
@@ -19,13 +50,37 @@ export default {
       jobs: []
     };
   },
+  computed: {
+    currentPage() {
+      return Number.parseInt(this.$route.query.page || '1');
+    },
+    previousPage() {
+      const previousPage = this.currentPage - 1;
+      const firstPage = 1;
+      const ShowPrev = previousPage >= firstPage ? previousPage : undefined;
+      return ShowPrev;
+    },
+    nextPage() {
+      const nextPage = this.currentPage + 1;
+      const lastPage = Math.ceil(this.jobs.length / 10);
+      const showNext = nextPage <= lastPage ? nextPage : undefined;
+      return showNext;
+    },
+    displayedJobs() {
+      const pageNumber = this.currentPage;
+      const firstJobIndex = (pageNumber - 1) * 10;
+      const lastJobIndex = pageNumber * 10;
+      return this.jobs.slice(firstJobIndex, lastJobIndex);
+    }
+  },
   mounted() {
     // Call the fetchData method when the component is mounted
     this.getData();
   },
   methods: {
     async getData() {
-      const response = await axios.get('http://localhost:3001/jobs');
+      const baseUrl = import.meta.env.VITE_APP_API_URL;
+      const response = await axios.get(`${baseUrl}/jobs`);
       this.jobs = response.data;
     }
   }
--- frontend/tests/unit/components/JobResults/JobListings.test.js
+++ frontend/tests/unit/components/JobResults/JobListings.test.js
@@ -5,21 +5,94 @@ import axios from 'axios';
 import JobListings from '@/components/JobResults/JobListings.vue';
 vi.mock('axios');
 describe('JobListings', () => {
-  it('fetchs jobs', () => {
-    axios.get.mockResolvedValue({ data: [] });
-    render(JobListings);
-    expect(axios.get).toHaveBeenCalledWith('http://localhost:3001/jobs');
+  const createRoute = (queryParams = {}) => ({
+    query: {
+      page: '5',
+      ...queryParams
+    }
   });
-  it('creates a job listing for every job', async () => {
-    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+
+  const renderJobListings = ($route) => {
     render(JobListings, {
       global: {
+        mocks: {
+          $route
+        },
         stubs: {
           RouterLink: RouterLinkStub
         }
       }
     });
+  };
+
+  it('fetchs jobs', () => {
+    axios.get.mockResolvedValue({ data: [] });
+    const $route = createRoute();
+    renderJobListings($route);
+    expect(axios.get).toHaveBeenCalledWith('http://myfakeapi:3001/jobs');
+  });
+
+  it('creates a job listing for every job', async () => {
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+    const $route = createRoute({ page: '1' });
+    renderJobListings($route);
+
     const jobListings = await screen.findAllByRole('listitem'); // find -> async & get->sync
-    expect(jobListings).toHaveLength(15);
+    expect(jobListings).toHaveLength(10);
+  });
+
+  describe('when params exclude page number', () => {
+    it('it dispalyes page number 1', () => {
+      const $route = createRoute({ page: undefined });
+      renderJobListings($route);
+      expect(screen.getByText('Page 1')).toBeInTheDocument();
+    });
+  });
+
+  describe('when params include page number', () => {
+    it('it dispalyes page number', () => {
+      const $route = createRoute({ page: '3' });
+      renderJobListings($route);
+      expect(screen.getByText('Page 3')).toBeInTheDocument();
+    });
+  });
+
+  describe('when user is on page 1 ', () => {
+    it('does not show link to previous page', async () => {
+      axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+      const $route = createRoute({ page: '1' });
+      renderJobListings($route);
+      await screen.findAllByRole('listitem');
+      const previosLink = screen.queryByRole('link', { name: /previous/i });
+      expect(previosLink).not.toBeInTheDocument();
+    });
+
+    it('shows link to next page', async () => {
+      axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+      const $route = createRoute({ page: '1' });
+      renderJobListings($route);
+      await screen.findAllByRole('listitem');
+      const nextLink = screen.queryByRole('link', { name: /next/i });
+      expect(nextLink).toBeInTheDocument();
+    });
+  });
+
+  describe('when user is in last page', () => {
+    it('does not show link to next page', async () => {
+      axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+      const $route = createRoute({ page: '2' });
+      renderJobListings($route);
+      await screen.findAllByRole('listitem');
+      const nextLink = screen.queryByRole('link', { name: /next/i });
+      expect(nextLink).not.toBeInTheDocument();
+    });
+    it('does show link to previous page', async () => {
+      axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+      const $route = createRoute({ page: '2' });
+      renderJobListings($route);
+      await screen.findAllByRole('listitem');
+      const previousLink = screen.queryByRole('link', { name: /previous/i });
+      expect(previousLink).toBeInTheDocument();
+    });
   });
 });
